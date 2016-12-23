af39560d1deabff9db66cc2518115be4f537ee26
seborama <sebastien.chatal@sainsburys.co.uk>
2016-12-23 17:28:12 +0000

    Filters files from text_000/ only

--- src/main/java/seborama/enron/Main.java
+++ src/main/java/seborama/enron/Main.java
@@ -8,10 +8,12 @@ import java.util.zip.ZipFile;
 public class Main {
     public static long OpenZipStream(String zip) throws IOException {
         Predicate<ZipEntry> isFile = ze -> !ze.isDirectory();
+        Predicate<ZipEntry> isInTextDirectory = ze -> ze.getName().contains("text_000/");
 
         ZipFile zipFile = new ZipFile(zip);
         return zipFile.stream()
                 .filter(isFile)
+                .filter(isInTextDirectory)
                 .count();
     }
 }
