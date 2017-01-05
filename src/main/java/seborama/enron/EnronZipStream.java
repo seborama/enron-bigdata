6690817529e53756b960466d5f565c0549dd124b
Seb C <seborama@users.noreply.github.com>
2017-01-05 15:49:07 +0000

    Count words in emails (#7)
    * [CountWordsInEmails] Counts total words in emails
    
    * [CountWordsInEmails] New approach currently coded 'blah'
    
    * [CountWordsInEmails] Count words in emails and calc average
    
    * [CountWordsInEmails] Removed unused parameter type
    
    * [CountWordsInEmails] TravaisCI fix

--- src/main/java/seborama/enron/EnronZipStream.java
+++ src/main/java/seborama/enron/EnronZipStream.java
@@ -1,35 +1,69 @@
 package seborama.enron;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.*;
+import java.util.*;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toMap;
+
+class KeyValue<K, V> {
+    public K key;
+    public V value;
+
+    public KeyValue(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
 
 public class EnronZipStream {
     public long OpenZipStream(String zip) throws IOException {
         Predicate<ZipEntry> isFile = ze -> !ze.isDirectory();
         Predicate<ZipEntry> isInTextDirectory = ze -> ze.getName().contains("text_000/");
-        Predicate<ZipEntry> isText = ze -> ze.getName().matches("^.*[A-Z]\\.txt$");
+        Predicate<ZipEntry> isTextEmail = ze -> ze.getName().matches("^.*[A-Z]\\.txt$");
 
         ZipFile zipFile = new ZipFile(zip);
-        return zipFile.stream()
-                .filter(isFile)
-                .filter(isInTextDirectory)
-                .filter(isText)
-                .map(ze -> getEmailBody(zipFile, ze))
-                .flatMap(Arrays::stream)
-                .count();
+
+//        System.out.println("DEBUG 2");
+//        return zipFile.stream()
+//                .filter(isFile.and(isTextEmail).and(isInTextDirectory))
+//                .map(ze -> getEmailBody(zipFile, ze))
+//                .flatMap(Arrays::stream)
+//                .map(line -> getWords(line))
+//                .flatMap(Arrays::stream)
+//                .count();
+
+        Map<String, Long> blah1b = zipFile.stream()
+                .filter(isFile.and(isTextEmail).and(isInTextDirectory))
+                .collect(toMap(ze -> ze.getName(), ze -> Stream.of(getEmailBody(zipFile, ze))
+                        .map(line -> getWords(line))
+                        .flatMap(Arrays::stream)
+                        .count()
+                ));
+
+        return new Double(blah1b.entrySet().stream()
+                .flatMapToLong(es -> LongStream.of(es.getValue()))
+                .average()
+                .getAsDouble()).longValue();
+    }
+
+    private String[] getWords(String line) {
+        Predicate<String> isNotEmpty = s -> s != null && !s.isEmpty();
+
+        return Stream.of(line.split("\\W+")).filter(isNotEmpty).toArray(String[]::new);
     }
 
     private String[] getEmailBody(ZipFile zipFile, ZipEntry zipEntry) {
         List<String> writer = new ArrayList<>();
 
+        // Unfortunately you have to close functional file streams explicitly with try/with statements.
         try (InputStream inputStream = zipFile.getInputStream(zipEntry);
              BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
 
