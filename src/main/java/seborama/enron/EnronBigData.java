7bbe25d141e90a4b2217c5e1ac1494e658b7dac7
Seb C <seborama@users.noreply.github.com>
2017-01-16 22:37:32 +0000

    [ForkJoinPool] Implementation of custom ForkJoinPool (#12)
    For parallelism

--- src/main/java/seborama/enron/EnronBigData.java
+++ src/main/java/seborama/enron/EnronBigData.java
@@ -4,25 +4,39 @@ import seborama.enron.stats.AverageWordsPerMail;
 import seborama.enron.stats.TopMailRecipients;
 
 import java.io.IOException;
+import java.util.concurrent.*;
 
 public class EnronBigData {
     public static void main(String[] args) throws IOException {
         if (args.length != 1)
             usage();
 
-//        setParallelism();
-
         AverageWordsPerMail averageWordsPerMail = new AverageWordsPerMail();
-        System.out.println("Average length, in words, of the emails\n(ignoring attachments):\n" + averageWordsPerMail.calculate(args[0]) + "\n\n");
+        System.out.println("Average length, in words, of the emails\n(ignoring attachments):\n" + wrapForkJoinPool(() -> averageWordsPerMail.calculate(args[0])) + "\n\n");
 
         TopMailRecipients topMailRecipients = new TopMailRecipients();
         System.out.println("Top 100 recipient email addresses\n(an email sent to N recipients would could N times - count “cc” as 50%):\n" + topMailRecipients.calculate(args[0]) + "\n\n");
     }
 
-    private static void setParallelism() {
-        int numCores = Runtime.getRuntime().availableProcessors() / 2;
-        System.out.printf("Setting ForkJoinPool parallelism to %d\n", numCores);
-        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", String.valueOf(numCores));
+    private static long wrapForkJoinPool(Callable<Long> task) {
+        final ForkJoinPool forkJoinPool = new ForkJoinPool(30);
+        ForkJoinTask<Long> fjt = forkJoinPool.submit(task);
+        try {
+            forkJoinPool.shutdown();
+            forkJoinPool.awaitTermination(1, TimeUnit.HOURS);
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+
+        long result = -1;
+        try {
+            result = fjt.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("DEBUG - returning result: " + result);
+        return result;
     }
 
     private static void usage() {
